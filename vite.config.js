74b957673d6b0ece046e372c6a2f7657f908e992
Tom Paton <tom.paton@gmail.com>
2024-02-14 22:05:33 +1100

    allow access to dev site from phone

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [
-    /* 
+    /*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
@@ -13,6 +13,7 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
+    host: true,
   },
   build: {
     target: 'esnext',
